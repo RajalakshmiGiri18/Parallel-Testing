8453167ffb025414f2dd22988d894cbe339fa1af
RajalakshmiGiri18 <rajalakshmigiri18@gmail.com>
2024-09-01 09:11:54 -0700

    First Commit

---
+++ Parallel Test TestNG suite/src/com/parallelTest/ParallelTestNG.java
@@ -0,0 +1,42 @@
+package com.parallelTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+ 
+public class ParallelTestNG {
+String baseUrl = "https://www.seleniumhq.org";
+WebDriver driver;
+ 
+@Test
+@Parameters("Browsers")
+public void LaunchChromeBrowser(String BrowserName) {
+  if (BrowserName.equalsIgnoreCase("Chrome")) {
+   System.setProperty("webdriver.chrome.driver", "./ChromeDriverJars/chromedriver.exe");
+   driver = new ChromeDriver();
+   driver.manage().window().maximize();
+   driver.get(baseUrl);
+   System.out.println(driver.getTitle());
+   driver.quit();
+  } else if (BrowserName.equalsIgnoreCase("Firefox")) {
+   System.setProperty("webdriver.gecko.driver", "./FirefoxDriver/geckodriver.exe");
+   driver = new FirefoxDriver();
+   driver.manage().window().maximize();
+   driver.get(baseUrl);
+   System.out.println(driver.getTitle());
+   driver.quit();
+  }
+ 
+  else if (BrowserName.equalsIgnoreCase("Edge")) {
+   System.setProperty("webdriver.edge.driver", "./EdgeBrowserJars/msedgedriver.exe");
+   driver = new EdgeDriver();
+   driver.manage().window().maximize();
+   driver.get(baseUrl);
+   System.out.println(driver.getTitle());
+   driver.quit();
+  }
+}
+}
